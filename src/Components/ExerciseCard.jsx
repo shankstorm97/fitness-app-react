8b2564c1ad59ce1f695b9dfb694ddda627f4f8d0
shankstorm <work.prabhdeepsinngh@gmail.com>
2023-02-21 11:41:33 +0530

    commit after completing exerciseCard component

--- src/Components/ExerciseCard.jsx
+++ src/Components/ExerciseCard.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { Button, Stack, Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 
-const ExerciseCard = () => {
+const ExerciseCard = ({ exercise }) => {
   return (
-    <div>ExerciseCard</div>
-  )
-}
+    <Link to={`/exercise/${exercise.id}`} className="exercise-card">
+      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      <Stack direction="row">
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            background: "#ffa9a9",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.bodyPart}
+        </Button>
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            background: "#fcc757",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.target}
+        </Button>
+      </Stack>
+      <Typography
+        marginLeft="21px"
+        color="black"
+        fontWeight="bold"
+        mt="11px"
+        pb="10px"
+        textTransform="capitalize"
+        fontSize="22px"
+      >
+        {exercise.name}
+      </Typography>
+    </Link>
+  );
+};
 
-export default ExerciseCard
+export default ExerciseCard;

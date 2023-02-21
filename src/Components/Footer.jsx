ac638ae566ba9bfb8ad517487dc7b8748f2983ed
shankstorm <work.prabhdeepsinngh@gmail.com>
2023-02-21 16:45:56 +0530

    final commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Box, Stack, Typography } from "@mui/material";
+import React from "react";
+import Logo from "../assets/images/Logo-1.png";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="" width="200px" height="40px" />
+        <Typography variant="h4" mt="40px" mb="20px">
+          Made by Prabhdeep Singh
+        </Typography>
+      </Stack>
+    </Box>
+  );
+};
 
-export default Footer
+export default Footer;

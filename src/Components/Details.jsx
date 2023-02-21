3f5585d7bf68b155c84f14d1cd33959bb401ec9f
shankstorm <work.prabhdeepsinngh@gmail.com>
2023-02-21 15:04:50 +0530

    commit after changes to Details component

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -2,16 +2,27 @@ import React from "react";
 import BodyPartImage from "../assets/icons/body-part.png";
 import TargetImage from "../assets/icons/target.png";
 import EquipmentImage from "../assets/icons/equipment.png";
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 
 const Details = ({ exerciseDetails }) => {
   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetails;
+  const extraDetail = [
+    { icon: BodyPartImage, name: bodyPart },
+    { icon: TargetImage, name: target },
+    { icon: EquipmentImage, name: equipment },
+  ];
   return (
     <Stack
       gap="60px"
       sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
     >
       <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+      <Stack sx={{ gap: { lg: "35p", xs: "20px" } }}>
+        <Typography>{name}</Typography>
+        <Typography variant="h6">
+          Exercise {name} focuses on building your {target}.
+        </Typography>
+      </Stack>
     </Stack>
   );
 };
